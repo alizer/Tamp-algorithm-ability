6918d1b477061e47c00ff50b45177f3aeade97b9
“wendi” <liudongya.ldy@antgroup.com>
2021-10-30 10:56:26 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Name:         tamp-fundation-algorithm
+# Author:       wendi
+# Date:         2021/10/19
+from Tree import TrieTree
+
+if __name__ == '__main__':
+    trie = TrieTree()
+    print(trie.insert("apple"))
+    print(trie.insert("fdd"))
+    print(trie.search("apple"))
+    print(trie.search("app"))
+    print(trie.startsWith("app"))
+    print(trie.insert("app"))
+    print(trie.search("app"))
+
+
+
